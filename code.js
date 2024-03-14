8531fd18e4ba74d3a3323727391eb268b293d4c2
ross223 <144744308+ross223@users.noreply.github.com>
2024-03-13 20:24:26 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,37 +1,35 @@
 function mergesort(array) {
     var length = array.length;
-    var first = true;
-    var j;
-    var i;
     if (length <= 1) {
         return array;
     } else {
-        for (var size = 1; size < length; size = 2 * size) { // These loops were written by me but ZachRenz helped me understand how to write them
-            j = size;
-            if (first){
-                j = 1;
-                first = false;
-            } 
-            i = 0;
-            while (j < length) {
-                merge(i, j, size, array);
-                i += 2 * size;
-                j += 2 * size;
+        for (var size = 1; size < length; size = 2 * size) {
+            var start = 0;
+            while (start <= length) {
+                var end = Math.min(start + 2 * size - 1, length - 1)
+                var mid = Math.min(size + start - 1, length - 1);
+                merge(start, mid, end, array);
+                start += 2 * size;
             }
         }
     }
     return array;
 }
 
-function merge(i, j, size, array) { // This merge function was also written by me minus the for loop
-    while (i < j) {
-        if (array[i] <= array[j]) {
-            i++;
+function merge(lstart, mid, end, array) {
+    var rstart = mid + 1;
+    while (lstart <= mid && rstart <= end) {
+        if (array[lstart] <= array[rstart]) {
+            lstart++;
         } else {
-            for (var k = j + 1; k <= (j - 1 + size) && array[k] < array[i]; k++) { // Chat GPT helped me with getting this specifc piece of logic to get the rest of the code working
+            var value = array[rstart];
+            for (var i = rstart; i >= lstart; i--) {
+                array[i] = array[i - 1];
             }
-            k--;
-            [array[i], array[k]] = [array[k], array[i]] // Used https://www.freecodecamp.org/news/swap-two-array-elements-in-javascript/ for help with swaping elements in an array
+            array[lstart] = value;
+            lstart++;
+            rstart++;
+            mid++;
         }
     }
 }
